6eab3e15c460dbd954aba6fd129bfcfb90ba3145
SHARATH KUMAR.M <sharathm6667@gmail.com>
2026-03-17 21:57:50 +0530

    updating App.js file

--- frontend/ai-learning-assistant/src/App.jsx
+++ frontend/ai-learning-assistant/src/App.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+
 
 const App = () => {
+  const isAuthenticated = false;
+  const loading = false;
+
+  if(loading) {
   return (
-    <div>APP</div>
-  )
+    <div className='flext items-center justify-center h-screen'>
+      <p>Loading....</p>
+    </div>
+    
+  );
+}
+
+return (
+  <Router>
+    <Routes>
+      <Route
+      path="/"
+      elemengt={isAuthenticated ? <Navigate to ="/dashboard" replace /> : <Navigate to="/login" replace />}
+      />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+
+
+
+      <Route path="*" element={<NotFoundPage />} />
+      f
+    </Routes>
+  </Router>
+)
 }
 
 export default App
