02cf93afdcb5f1f563c228a7c4ad150f40ecb239
SHARATH KUMAR.M <sharathm6667@gmail.com>
2026-03-24 21:53:35 +0530

    intialize backend

---
+++ frontend/ai-learning-assistant/src/components/layout/AppLayout.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AppLayout = () => {
+    return (
+        <div>
+            <h1>AI Learning Assistant</h1>
+        </div>
+    )
+}
+
+export default AppLayout
